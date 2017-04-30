00d0a3fd0349984fcef77151f1a5245732db4dc9
Nathan Osman <nathan@quickmediasolutions.com>
2017-04-29 22:15:08 -0700

    Add code for loading and generating private keys.

---
+++ key.go
@@ -0,0 +1,41 @@
+package simpleacme
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"io/ioutil"
+)
+
+const keyType = "RSA PRIVATE KEY"
+
+var ErrInvalidKey = errors.New("invalid key")
+
+// loadKey attempts to load a private key from the specified file.
+func loadKey(filename string) (*rsa.PrivateKey, error) {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	block, _ := pem.Decode(b)
+	if block == nil || block.Type != keyType {
+		return nil, ErrInvalidKey
+	}
+	return x509.ParsePKCS1PrivateKey(block.Bytes)
+}
+
+// generateKey creates a new 2048-bit RSA key and writes it to the specified
+// file.
+func generateKey(filename string) error {
+	k, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return err
+	}
+	b := pem.EncodeToMemory(&pem.Block{
+		Type:  keyType,
+		Bytes: x509.MarshalPKCS1PrivateKey(k),
+	})
+	return ioutil.WriteFile(filename, b, 0600)
+}
